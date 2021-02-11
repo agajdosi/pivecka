8a89dd91cb6a31ae414d0f46aee90e292d2d9232
agajdosi <andreas.gajdosik@gmail.com>
2021-02-11 17:08:11 +0100

    Adding basic texts and images

---
+++ script.js
@@ -0,0 +1,20 @@
+texts = [
+  "Nenechte zkrachovat\nčeský národní sport!",
+  "Pomožte nám vypít sudy\nnenechte nás v tom!"
+]
+
+
+var ad = document.getElementsByClassName("pivoad")[0];
+
+
+
+ad.style.backgroundImage = "url(https://pics.ytro.ru/utro_photos/2020/03/14/1438587.jpg)";
+ad.style.backgroundSize = "cover"; //pak mozna vyresit vix flexibilne orezavani
+//tady vyresit cernobilost
+
+//TEXT
+text1 = ad.getElementsByClassName("text1")[0];
+text2 = ad.getElementsByClassName("text2")[0];
+
+
+
